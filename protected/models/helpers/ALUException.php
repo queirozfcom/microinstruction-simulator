816725bda5ca0752db85e94a92912de2c55ae3b2
Felipe <felipe@felipe-XPS-8300.(none)>
2012-06-02 23:39:59 -0300

    forgot a few models....

---
+++ protected/models/helpers/ALUException.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ALUException
+ *
+ * @author felipe
+ */
+class ALUException extends Exception{
+    //put your code here
+}
+
+?>
