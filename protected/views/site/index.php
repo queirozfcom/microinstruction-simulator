896bcea6dce7afebdd7f3432c09aeb7bb64ba637
Felipe Almeida <blue_tree1988@yahoo.com>
2013-04-21 05:00:07 -0300

    button now points to github repository, page title is correct

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,6 +1,6 @@
 <div class="container">
     <div class="well" style="text-align:center;">
-        
+
         <div class="row-fluid">
 
             <h2>Microinstruction Simulator</h2>
@@ -11,58 +11,60 @@
             <br />
             <div class="row-fluid">
 
-                    <?php echo CHtml::htmlButton('Write a Program', [
-                        'class' => 'btn btn-large btn-primary span4 offset4',
-                        'style'=>'height:70px;',
-                        'onclick'=>'window.location.href="' . Yii::app()->createUrl('site/write') . '";'
-                        ]); ?>
-                    <br /><br />
-                    
+                <?php
+                echo CHtml::htmlButton('Write a Program', [
+                    'class' => 'btn btn-large btn-primary span4 offset4',
+                    'style' => 'height:70px;',
+                    'onclick' => 'window.location.href="' . Yii::app()->createUrl('site/write') . '";'
+                ]);
+                ?>
+                <br /><br />
+
 
             </div>
             <br />
             <br />
-            <br />
-            <br />
             <div class="row-fluid">
 
+                <div>
 
-                    <p>Coming soon:</p>
-                    <div>
-                        <?php
-                        echo CHtml::htmlButton('Introduction Video', [
-                            'class' => 'btn btn-large btn-info span4 offset4',
-                            'disabled' => "disabled",
-                        ]);
-                        ?>
-                        <br /><br /><br />
-                    </div>
+                    <?php
+                    echo CHtml::htmlButton('Source Code', [
+                        'class' => 'btn btn-large btn-success span4 offset4',
+                        'onclick' => "window.location='https://github.com/queirozfcom/microinstruction-simulator'",
+                    ]);
+                    ?>
 
-                    <div>
+                </div>
+                <br />
+                <br />
+                <br />
+                <br />
 
-                        <?php
-                        echo CHtml::htmlButton('Documentation', [
-                            'class' => 'btn btn-large btn-info span4 offset4',
-                            'disabled' => "disabled",
-                        ]);
-                        ?>
+                <p>Coming soon:</p>
+                <div>
+                    <?php
+                    echo CHtml::htmlButton('Introduction Video', [
+                        'class' => 'btn btn-large btn-info span4 offset4',
+                        'disabled' => "disabled",
+                    ]);
+                    ?>
+                    <br /><br /><br />
+                </div>
 
-                    </div>
-                    <br />
-                    <br />
-                    <br />
-                    <div>
+                <div>
 
-                        <?php
-                        echo CHtml::htmlButton('Source Code', [
-                            'class' => 'btn btn-large btn-info span4 offset4',
-                            'disabled' => "disabled",
-                        ]);
-                        ?>
+                    <?php
+                    echo CHtml::htmlButton('Documentation', [
+                        'class' => 'btn btn-large btn-info span4 offset4',
+                        'disabled' => "disabled",
+                    ]);
+                    ?>
 
-                    </div>
-                    <br />
-                    <br />
+                </div>
+
+                <br />
+                <br />
 
             </div>
 
